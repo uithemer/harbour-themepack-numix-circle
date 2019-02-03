e0065c85bd7e8ffd1ec16f6cebf02ec978d5736b
fravaccaro <fravaccaro90@gmail.com>
2019-02-04 00:38:58 +0100

    updated translations

--- translations/harbour-themepack-numix-circle-sl_SI.ts
+++ translations/harbour-themepack-numix-circle-sl_SI.ts
@@ -81,11 +81,11 @@
     </message>
     <message>
         <source>Credits</source>
-        <translation type="unfinished"></translation>
+        <translation>Zasluge</translation>
     </message>
     <message>
         <source>Keyboard navigation based on the one on &lt;a href=&apos;https://github.com/Wunderfitz/harbour-piepmatz&apos;&gt;Piepmatz&lt;/a&gt; by Sebastian Wolf.</source>
-        <translation type="unfinished"></translation>
+        <translation>Upravljanje s tipkovnico je povzeto po programu &lt;a href=&apos;https://github.com/Wunderfitz/harbour-piepmatz&apos;&gt;Piepmatz&lt;/a&gt;, avtor Sebastian Wolf.</translation>
     </message>
 </context>
 </TS>
